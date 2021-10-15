0471c43a91b46c79d1a5671b0f42d5b6c1137dfb
Tobias Brandy <tbrandy@itba.edu.ar>
2021-10-15 15:28:38 -0300

    Setup de proyecto maven

--- server/src/main/java/ar/edu/itba/pod/server/Server.java
+++ server/src/main/java/ar/edu/itba/pod/server/Server.java
@@ -1,7 +1,18 @@
 package ar.edu.itba.pod.server;
 
+import java.util.Collections;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.config.GroupConfig;
+import com.hazelcast.config.InterfacesConfig;
+import com.hazelcast.config.JoinConfig;
+import com.hazelcast.config.ManagementCenterConfig;
+import com.hazelcast.config.MulticastConfig;
+import com.hazelcast.config.NetworkConfig;
+import com.hazelcast.core.Hazelcast;
 
 public class Server {
     private static Logger logger = LoggerFactory.getLogger(Server.class);
