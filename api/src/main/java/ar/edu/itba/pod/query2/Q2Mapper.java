1aae5cca422b619ce0d2b43e8921e72a3604abf1
Tobias Brandy <tbrandy@itba.edu.ar>
2021-10-17 10:59:34 -0300

    Q2Mapper: estilo

--- api/src/main/java/ar/edu/itba/pod/query2/Q2Mapper.java
+++ api/src/main/java/ar/edu/itba/pod/query2/Q2Mapper.java
@@ -2,9 +2,10 @@ package ar.edu.itba.pod.query2;
 
 import static java.util.Objects.*;
 
+import java.util.Map;
+
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.HazelcastInstanceAware;
-import com.hazelcast.core.IMap;
 import com.hazelcast.mapreduce.Context;
 import com.hazelcast.mapreduce.Mapper;
 
@@ -14,24 +15,21 @@ import ar.edu.itba.pod.models.Tree;
 public class Q2Mapper implements Mapper<String, Tree, Neighbourhood, String>, HazelcastInstanceAware {
 
     // Configuration
-    private final String                        mapName;
+    private final   String                        mapName;
 
-    private       IMap<String, Neighbourhood>   hoodMap;
+    private         Map<String, Neighbourhood>    hoodMap;
 
     public Q2Mapper(final String mapName) {
         this.mapName = requireNonNull(mapName);
     }
 
     @Override
-    public void setHazelcastInstance(HazelcastInstance hazelcast) {
+    public void setHazelcastInstance(final HazelcastInstance hazelcast) {
         this.hoodMap = hazelcast.getMap(mapName);
-        
     }
-
 
     @Override
-    public void map(String hoodName, Tree tree, Context<Neighbourhood, String> context) {
+    public void map(final String hoodName, final Tree tree, final Context<Neighbourhood, String> context) {
         context.emit(hoodMap.get(hoodName), tree.getName());
     }
-    
 }
