b8016c55c994808b24c6add634042ae6129d3e1c
Tobias Brandy <tbrandy@itba.edu.ar>
2021-10-17 21:34:52 -0300

    Datasources: no usamos Function, directamente metodos abstractos

--- api/src/main/java/ar/edu/itba/pod/models/DataSources.java
+++ api/src/main/java/ar/edu/itba/pod/models/DataSources.java
@@ -1,31 +1,30 @@
 package ar.edu.itba.pod.models;
 
-import java.util.function.Function;
-
 public enum DataSources {
-    BUE(
-        csv -> new Tree(csv[2], csv[4], csv[7]),
-        csv -> new Neighbourhood(csv[0], Long.parseLong(csv[1]))
-    ),
-    VAN(
-        csv -> new Tree(csv[2], csv[6], csv[12]),
-        csv -> new Neighbourhood(csv[0], Long.parseLong(csv[1]))
-    ),
+    BUE {
+        @Override
+        public Tree treeFromCSV(final String[] csv) {
+            return new Tree(csv[2], csv[4], csv[7]);
+        }
+
+        @Override
+        public Neighbourhood hoodFromCSV(final String[] csv) {
+            return new Neighbourhood(csv[0], Long.parseLong(csv[1]));
+        }
+    },
+    VAN {
+        @Override
+        public Tree treeFromCSV(final String[] csv) {
+            return new Tree(csv[2], csv[6], csv[12]);
+        }
+
+        @Override
+        public Neighbourhood hoodFromCSV(final String[] csv) {
+            return new Neighbourhood(csv[0], Long.parseLong(csv[1]));
+        }
+    },
     ;
 
-    final Function<String[], Tree>          treeBuilder;
-    final Function<String[], Neighbourhood> neighbourhoodBuilder;
-
-    DataSources(final Function<String[], Tree> treeBuilder, final Function<String[], Neighbourhood> neighbourhoodBuilder) {
-        this.treeBuilder            = treeBuilder;
-        this.neighbourhoodBuilder   = neighbourhoodBuilder;
-    }
-
-    public Tree treeFromCSV(final String[] csvLine) {
-        return treeBuilder.apply(csvLine);
-    }
-
-    public Neighbourhood neighbourhoodFromCSV(final String[] csvLine) {
-        return neighbourhoodBuilder.apply(csvLine);
-    }
+    public abstract Tree            treeFromCSV(final String[] csvLine);
+    public abstract Neighbourhood   hoodFromCSV(final String[] csvLine);
 }
