6ed5f03a894bdc93928c6317d72fe18580a59bbc
Tobias Brandy <tbrandy@itba.edu.ar>
2021-10-21 01:27:33 -0300

    DistinctValuesCountReducerFactory: cuenta la cantidad de valores distintos
    Recibe coleccion pues asume combiner previo

---
+++ api/src/main/java/ar/edu/itba/pod/DistinctValuesCountReducerFactory.java
@@ -0,0 +1,36 @@
+package ar.edu.itba.pod;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.hazelcast.mapreduce.Reducer;
+import com.hazelcast.mapreduce.ReducerFactory;
+
+public class DistinctValuesCountReducerFactory<Key, Values extends Collection<?>> implements ReducerFactory<Key, Values, Integer> {
+
+    @Override
+    public DistinctValuesCountReducer<Values> newReducer(final Key key) {
+        return new DistinctValuesCountReducer<>();
+    }
+    
+    private static class DistinctValuesCountReducer<Values extends Collection<?>> extends Reducer<Values, Integer> {
+
+        private Set<Object> distinctValues;
+
+        @Override
+        public void beginReduce() {
+            distinctValues = new HashSet<>();
+        }
+
+        @Override
+        public void reduce(final Values values) {
+            distinctValues.addAll(values);
+        }
+        
+        @Override
+        public Integer finalizeReduce() {
+            return distinctValues.size();
+        }
+    }
+}
