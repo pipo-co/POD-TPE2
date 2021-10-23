48720b69bf05caee700dc9e2c2973227e273129c
Tobias Brandy <tbrandy@itba.edu.ar>
2021-10-23 02:03:01 -0300

    Primer iteracion test query 1
    Falla un caso

--- client/src/test/java/queries/Query1Test.java
+++ client/src/test/java/queries/Query1Test.java
@@ -2,20 +2,49 @@ package queries;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.Map;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
+
+import ar.edu.itba.pod.client.queries.Query1;
+import ar.edu.itba.pod.models.Neighbourhood;
+import ar.edu.itba.pod.models.Tree;
+import ar.edu.itba.pod.query1.Q1Answer;
 
 public class Query1Test extends AbstractQueryTest {
 
     @Test
-    public void test_query_1() {
-        final Map<String, String> map = client.getMap("hola");
+    public void test_query_1() throws ExecutionException, InterruptedException {
 
-        assertFalse(map.containsKey("test"));
-        map.put("test", "1");
+        final Stream<Neighbourhood> hoods = Stream.of(
+            new Neighbourhood("1", 30),
+            new Neighbourhood("2", 30),
+            new Neighbourhood("3", 30),
+            new Neighbourhood("4", 30)
+        ).unordered();
 
-        assertTrue(map.containsKey("test"));
-        assertEquals("1", map.get("test"));
+        final Stream<Tree> trees = Stream.of(
+            new Tree("2", "1-street", "t1"),
+            new Tree("1", "2-street", "t2"),
+            new Tree("3", "3-street", "t3"),
+            new Tree("3", "4-street", "t4"),
+            new Tree("1", "5-street", "t5"),
+            new Tree("7", "5-street", "t6")
+        ).unordered();
+
+        final List<Q1Answer> answers = new LinkedList<>();
+
+        Query1.execute(client, trees, hoods, answers::add);
+
+        final List<Q1Answer> expectedAnswers = List.of(
+            new Q1Answer("1", 2),
+            new Q1Answer("3", 2),
+            new Q1Answer("2", 1),
+            new Q1Answer("4", 0)
+        );
+        assertEquals(expectedAnswers, answers);
     }
 }
